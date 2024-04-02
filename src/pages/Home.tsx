ee35796fe4a9290a172f8e78a3c3d9137a557861
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2024-04-01 22:06:51 -0300

    feat: Attendee List Table
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,5 @@
 import { AttendeeList } from '@/components/Attendee-List';
 
 export function Home() {
-  return (
-    <section>
-      <AttendeeList />
-    </section>
-  );
+  return <AttendeeList />;
 }
