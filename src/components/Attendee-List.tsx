b660576b73f9e2f68b8ec01f38fa27bdd1c5ff12
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2024-04-03 21:29:30 -0300

    feat: URL state pagination
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/components/Attendee-List.tsx
+++ src/components/Attendee-List.tsx
@@ -27,7 +27,14 @@ interface AttendeeProps {
 
 export function AttendeeList() {
   const [search, setSearch] = useState('');
-  const [page, setPage] = useState(1);
+  const [page, setPage] = useState(() => {
+    const url = new URL(window.location.toString());
+    if (url.searchParams.has('page')) {
+      return Number(url.searchParams.get('page'));
+    }
+
+    return 1;
+  });
   const [total, setTotal] = useState(0);
   const [attendees, setAttendees] = useState<AttendeeProps[]>([]);
 
@@ -51,25 +58,33 @@ export function AttendeeList() {
       });
   }, [page, search]);
 
+  function setCurrentPage(page: number) {
+    const url = new URL(window.location.toString());
+    url.searchParams.set('page', String(page));
+    window.history.pushState({}, '', url);
+
+    setPage(page);
+  }
+
   function onSearchInputChanged(event: ChangeEvent<HTMLInputElement>) {
     setSearch(event.target.value);
-    setPage(1);
+    setCurrentPage(1);
   }
 
   function goToFirstPage() {
-    setPage(1);
+    setCurrentPage(1);
   }
 
   function goToPreviousPage() {
-    setPage(page - 1);
+    setCurrentPage(page - 1);
   }
 
   function goToNextPage() {
-    setPage(page + 1);
+    setCurrentPage(page + 1);
   }
 
   function goToLastPage() {
-    setPage(totalPages);
+    setCurrentPage(totalPages);
   }
 
   return (
